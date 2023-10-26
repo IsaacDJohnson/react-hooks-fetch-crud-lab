09654af9f0d0dc5d882e49ae85e396aadfdbcbf0
IsaacDJohnson <isaac.designws@gmail.com>
2023-10-26 11:10:52 -0500

    completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({data}) {
+
+  
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/questions')
+      .then(r => r.json())
+      .then((data) => {
+        setQuestions(data)
+    })
+  }, [])
+
+  function onDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`,{method: 'DELETE'})
+    .then(res => res.json())
+    .then(() => { 
+      const newQuestion = questions.filter(ques => ques.id!= id)
+      setQuestions(newQuestion)
+    })
+  }
+
+  function handleAnswer(id, correctIndex){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: 'PATCH', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({correctIndex}),
+    })
+    .then(data=>data.json())
+    .then(data=>{
+  
+  const updated=questions.map(item=>{
+    if(item.id===data.id){
+      return data
+    } return item
+  })
+    console.log(updated,"after upadat");
+    setQuestions(updated)  
+    })
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions ? questions.map((item, i)=> <QuestionItem key={i} onSelectChange={handleAnswer} question={item} onDelete={()=> onDelete(item.id)}/>):[]}
+      </ul>
     </section>
   );
 }
